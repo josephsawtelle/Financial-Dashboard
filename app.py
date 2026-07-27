a5962c82603f477392b48187df529d35dedf8364
josephsawtelle <josephsawtelle2004@gmail.com>
2026-07-27 16:17:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ st.set_page_config(
     page_icon="📊",
     layout="wide",
 )
-st.logo("Flatiorns Financial Insights Logo.png")
 st.image("Flatiorns Financial Insights Logo.png", width=250)
 
 REQUIRED_TRANSACTION_COLUMNS = {
